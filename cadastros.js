0b68965b4620bd82868299ca9440d5f5a3121b13
diogobgf <116925404+diogobgf@users.noreply.github.com>
2023-11-13 23:53:37 +0000

    cadastros

---
+++ cadastros.js
@@ -0,0 +1,12 @@
+//FAP - Turma 2: JavaScript e Orientação a Objetos - Trabalho Extra 01 #94361
+
+var nome = "Joâo";
+var salario = 2000;
+var idade = 35;
+var diploma = false;
+
+
+ console.log("O nome é", nome, "e a variável e do tipo", typeof(nome));
+ console.log("O salario é", salario, "e a variável e do tipo", typeof(salario));
+ console.log("A idade é", idade, "e a variável e do tipo", typeof(idade));
+ console.log("Tem diploma?", diploma, "e a variável e do tipo", typeof(diploma));
