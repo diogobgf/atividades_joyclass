894dd47344a34777792bfff3b5ce33a0fe6ef418
diogobgf <116925404+diogobgf@users.noreply.github.com>
2023-11-17 14:25:06 +0000

    funcao2

--- funcoes.js
+++ funcoes.js
@@ -1,16 +1,21 @@
 // FAP - Turma 2: JavaScript e Orientação a Objetos - Trabalho 2 #94367
 
 // Função sem Parâmetros:
-function minhafuncao(){
+function minhaFuncao(){
     return console.log("Minha Função");
 }
-minhafuncao();
+minhaFuncao();
+
 
 // Função com Parâmetros:
-function mediaaluno(nota1, nota2) {
+function mediaAluno(nota1, nota2) {
     return (nota1 + nota2) / 2;
     
 }
 
-console.log(`Minha Média é ${mediaaluno(8,5)}`);
+console.log(`A Média é: ${mediaAluno(8,7)}`);
+
+// Arrow Functions
+const mediaAluno2 = (nt1, nt2) => console.log("A Média é: " + eval(nt1 + nt2)/2); 
+mediaAluno2(8, 7);
 
