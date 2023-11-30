be3f5b2f2d7cf1a59110b2b1a8c22c4a427da98b
diogobgf <116925404+diogobgf@users.noreply.github.com>
2023-11-30 12:44:17 +0000

    class2

--- class.js
+++ class.js
@@ -1,10 +1,20 @@
+//Trabalho 01 (POO) #94373
+
 class Carro {
     constructor(marca, modelo, ano){
         this.marca = marca;
         this.modelo = modelo;
         this.ano = ano;
-    }
+
+        acelerar (){
+            
+        }
+     }
+
+     
 }
 
 const carro1 = new Carro ("fiat", "Tempra", 1999);
-const carro2 = new Carro ("Ford", "Munstang")
+const carro2 = new Carro ("Ford", "Munstang", 1967);
+
+console.log(carro1, carro2); 
